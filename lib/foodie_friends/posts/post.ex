464df240287e0f9164b4b218b8014dde8f851361
monaroo <93936047+monaroo@users.noreply.github.com>
2023-07-31 14:40:48 -0600

    Co-authored-by: Mario Vellandi <mvellandi@users.noreply.github.com>

--- lib/foodie_friends/posts/post.ex
+++ lib/foodie_friends/posts/post.ex
@@ -10,14 +10,17 @@ defmodule FoodieFriends.Posts.Post do
 
     has_many :comments, FoodieFriends.Comments.Comment
 
+    belongs_to :user, FoodieFriends.Accounts.User
+
     timestamps()
   end
 
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:title, :content, :visible, :published_on])
-    |> validate_required([:title, :content, :visible, :published_on])
+    |> cast(attrs, [:title, :content, :visible, :published_on, :user_id])
+    |> validate_required([:title, :content, :visible, :published_on, :user_id])
     |> unique_constraint(:title)
+    |> foreign_key_constraint(:user_id)
   end
 end
