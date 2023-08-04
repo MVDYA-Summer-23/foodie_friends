90c8d274addf9d010f0053ecbb3fd9f12faedba8
stebbinsi2 <stebbinsi2@gmail.com>
2023-08-04 14:58:20 -0400

    Changed error message, redirected to same post pag

--- lib/foodie_friends_web/controllers/comment_controller.ex
+++ lib/foodie_friends_web/controllers/comment_controller.ex
@@ -6,10 +6,10 @@ defmodule FoodieFriendsWeb.CommentController do
   alias FoodieFriends.Posts
 
   # TODO: Check if this action is even necessary given that posts should include comment changesets for comment forms
-  def new(conn, _params) do
-    changeset = Comments.change_comment(%Comment{})
-    render(conn, :new, changeset: changeset)
-  end
+  # def new(conn, _params) do
+  #   changeset = Comments.change_comment(%Comment{})
+  #   render(conn, :new, changeset: changeset)
+  # end
 
   def create(conn, %{"comment" => comment_params}) do
     case Comments.create_comment(comment_params) do
@@ -20,15 +20,17 @@ defmodule FoodieFriendsWeb.CommentController do
 
       {:error, %Ecto.Changeset{} = comment_changeset} ->
         post = Posts.get_post!(comment_params["post_id"])
-        render(conn, :show, post: post, comment_changeset: comment_changeset)
+        conn
+        |> put_flash(:error, "Comment not created. Errors?")
+        |> redirect(to: ~p"/posts/#{post.id}")
     end
   end
 
-  def edit(conn, %{"id" => id}) do
-    comment = Comments.get_comment!(id)
-    changeset = Comments.change_comment(comment)
-    render(conn, :edit, comment: comment, changeset: changeset)
-  end
+  # def edit(conn, %{"id" => id}) do
+  #   comment = Comments.get_comment!(id)
+  #   changeset = Comments.change_comment(comment)
+  #   render(conn, :edit, comment: comment, changeset: changeset)
+  # end
 
   def update(conn, %{"id" => id, "comment" => comment_params}) do
     comment = Comments.get_comment!(id)
